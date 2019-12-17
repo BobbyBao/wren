a001967cbfae27bdd2172a284b9459903da7d372
baozhenhua <baozhenhua@corp.netease.com>
2019-12-17 20:31:23 +0800

    remove io module

--- src/module/io.h
+++ src/module/io.h
@@ -2,10 +2,11 @@
 #define io_h
 
 #include "wren.h"
+#if 0
 
 // Frees up any pending resources in use by the IO module.
 //
 // In particular, this closes down the stdin stream.
 void ioShutdown();
-
+#endif
 #endif
